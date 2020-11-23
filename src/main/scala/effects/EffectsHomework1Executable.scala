172cf0154937e9031a6a389898961145592b5ebf
Maris Svirksts <maris.svirksts@gmail.com>
2020-11-23 20:54:06 +0200

    2nd attempt

--- src/main/scala/effects/EffectsHomework1Executable.scala
+++ src/main/scala/effects/EffectsHomework1Executable.scala
@@ -1,5 +1,6 @@
 package effects
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
 
@@ -28,40 +29,52 @@ import scala.util.{Failure, Success, Try}
  */
 object EffectsHomework1Executable {
   final class IO[A] private (val run: () => A) {
-    def map[B](f: A => B): IO[B] = IO(f(run()))
-    def flatMap[B](f: A => IO[B]): IO[B] = this.map(f).run()
+    def map[B](f: A => B): IO[B] = IO(f(unsafeRunSync()))
+    def flatMap[B](f: A => IO[B]): IO[B] = this.map(f).unsafeRunSync()
     def *>[B](another: IO[B]): IO[B] = this.flatMap(x => another)
     def as[B](newValue: => B): IO[B] = this.map(_ => newValue)
     def void: IO[Unit] = this as ()
-    def attempt: IO[Either[Throwable, A]] = ???
-    def option: IO[Option[A]] = ???
-    def handleErrorWith[AA >: A](f: Throwable => IO[AA]): IO[AA] = ???
-    def redeem[B](recover: Throwable => B, map: A => B): IO[B] = ???
-    def redeemWith[B](recover: Throwable => IO[B], bind: A => IO[B]): IO[B] =
-      ???
+    def attempt: IO[Either[Throwable, A]] = IO(Try(unsafeRunSync()).toEither)
+    def option: IO[Option[A]] = IO(Try(unsafeRunSync()) match {
+      case Failure(e) => None
+      case Success(v) => Some(v)
+    })
+    def handleErrorWith[AA >: A](f: Throwable => IO[AA]): IO[AA] = IO(Try(unsafeRunSync()) match {
+      case Failure(e) => f(e).unsafeRunSync()
+      case Success(v) => v
+    })
+    def redeem[B](recover: Throwable => B, map: A => B): IO[B] = IO(Try(unsafeRunSync()) match {
+      case Failure(e) => recover(e)
+      case Success(v) => map(v)
+    })
+    def redeemWith[B](recover: Throwable => IO[B], bind: A => IO[B]): IO[B] = attempt.unsafeRunSync() match {
+      case Left(v) => recover(v)
+      case Right(v) => bind(v)
+    }
     def unsafeRunSync(): A = run()
-    def unsafeToFuture(): Future[A] = ???
+    def unsafeToFuture(): Future[A] = Future(unsafeRunSync())
   }
 
   object IO {
     def apply[A](body: => A): IO[A] = delay(body)
-    def suspend[A](thunk: => IO[A]): IO[A] = ???
+    def suspend[A](thunk: => IO[A]): IO[A] = apply(thunk.run())
     def delay[A](body: => A): IO[A] = new IO[A](() => body)
-    def pure[A](a: A): IO[A] = ???
-    def fromEither[A](e: Either[Throwable, A]): IO[A] = ???
+    def pure[A](a: A): IO[A] = new IO(() => a)
+    def fromEither[A](e: Either[Throwable, A]): IO[A] = IO(e match {
+      case Left(e) => throw e
+      case Right(v) => v
+    })
 
     def fromOption[A](option: Option[A])(orElse: => Throwable): IO[A] =
       IO(option.getOrElse(throw orElse))
 
-    // https://docs.scala-lang.org/overviews/scala-book/match-expressions.html
-    // https://docs.scala-lang.org/overviews/scala-book/try-catch-finally.html
     def fromTry[A](t: Try[A]): IO[A] =
       IO(t match {
         case Success(v) => v
         case Failure(e) => throw e
       })
 
-    def none[A]: IO[Option[A]] = ???
+    def none[A]: IO[Option[A]] = pure(None)
     def raiseError[A](e: Throwable): IO[A] = throw e
     def raiseUnless(cond: Boolean)(e: => Throwable): IO[Unit] =
       IO(if (!cond) throw e)
